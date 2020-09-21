66d7b48f1c38290eb080ef112e9a84ed68aa85dd
mohamadbastin <mohamadbastin@gmail.com>
2020-09-21 10:56:13 +0330

    initial commit

--- apiv1/models.py
+++ apiv1/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    student_no = models.IntegerField()
+    team = models.ForeignKey("Team", on_delete=models.CASCADE)
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=10, default="team")
+    presentation = models.OneToOneField("Presentation", on_delete=models.CASCADE)
+
