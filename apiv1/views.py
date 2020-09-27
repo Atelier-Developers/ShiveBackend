3bb8bf85dc56c997bf902e7a1d8700e31524ed55
mohamadbastin <mohamadbastin@gmail.com>
2020-09-27 18:09:53 +0330

    file upload

--- apiv1/views.py
+++ apiv1/views.py
@@ -498,6 +498,7 @@ class FileUploadView(APIView):
 
     def post(self, request, pk, format=None):
         # file_obj = request.FILES['file']
+        print("bzo boz e qandi")
         # # do some stuff with uploaded file
         # File.objects.create(file=file_obj, name=self.request.data.get("name"), link=" sfg",
         #                     presentation=Presentation.objects.get(pk=self.kwargs.get("pk")))
