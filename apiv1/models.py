d4fbcb1786ac3893276fab923d563b957b0a675c
mohamadbastin <mohamadbastin@gmail.com>
2020-09-29 21:46:22 +0330

    date time for file

--- apiv1/models.py
+++ apiv1/models.py
@@ -72,7 +72,7 @@ class File(models.Model):
     size = models.CharField(max_length=123, default=" ")
     presentation = models.ForeignKey(Presentation, on_delete=models.CASCADE, related_name="files")
     profile = models.ForeignKey(Profile, on_delete=models.SET_NULL, related_name='files', null=True, blank=True)
-    date = models.DateTimeField(auto_now_add=True, default=datetime.datetime.now())
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
