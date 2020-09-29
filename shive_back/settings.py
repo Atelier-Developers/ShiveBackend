2785c36b67aea5fba13b2c529b5386ba84350feb
mohamadbastin <mohamadbastin@gmail.com>
2020-09-29 20:23:54 +0330

    cdn config

--- shive_back/settings.py
+++ shive_back/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/shive/'
+MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = '/var/www/cdn/shive'
 
